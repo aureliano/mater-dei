ecbdb8478420bd687e8f708fc1561d8b2f7fcf2a
aureliano <aureliano.franca@hotmail.com>
2013-04-23 17:08:29 -0300

    Refatora parser de estórias.

--- src/main/java/br/materdei/bdd/jbehave/StoryNameParser.java
+++ src/main/java/br/materdei/bdd/jbehave/StoryNameParser.java
@@ -9,13 +9,31 @@ public class StoryNameParser {
 	}
 	
 	public static String parse(String storyPath) {
+		if (!storyPath.equals(storyPath.toLowerCase())) {
+			throw new RuntimeException("Formato de nome de arquivo inválido. O nome do arquivo deve ser todo minúsculo e separado com underline '_'.");
+		}
+		
 		File root = new File("");
 		storyPath = storyPath.replaceFirst(root.getAbsolutePath(), "");
 		storyPath = storyPath.replaceFirst("/src/(main|test)/resources/", "");
 		storyPath = storyPath.replaceAll("/", ".");
-		storyPath = storyPath.replaceAll("(.story|.estoria)", "");
+		storyPath = storyPath.replaceAll("(.story|.estoria)", "");		
 		
+		return convertToCamelCase(storyPath) + "Test";
+	}
+	
+	private static String convertToCamelCase(String text) {
+		String prefix = text.substring(0, text.lastIndexOf(".") + 1);
+		text = text.substring(text.lastIndexOf(".") + 1);
 		
-		return storyPath;
+		String[] tokens = text.split("_");
+		StringBuilder builder = new StringBuilder();
+		
+		for (String token : tokens) {
+			token = String.valueOf(token.charAt(0)).toUpperCase() + token.substring(1);
+			builder.append(token);
+		}
+		
+		return prefix + builder.toString();
 	}
 }
--- src/test/java/br/materdei/bdd/jbehave/StoryNameParserTest.java
+++ src/test/java/br/materdei/bdd/jbehave/StoryNameParserTest.java
@@ -10,11 +10,18 @@ public class StoryNameParserTest {
 	@Test
 	public void testParse() {
 		String root = new File("").getAbsolutePath();
-		assertEquals("br.materdei.bdd.feature.BddTest", StoryNameParser.parse(root + "/src/main/resources/br/materdei/bdd/feature/BddTest.story"));
-		assertEquals("br.materdei.bdd.feature.BddTest", StoryNameParser.parse(root + "/src/test/resources/br/materdei/bdd/feature/BddTest.story"));
-		assertEquals("br.materdei.bdd.feature.BddTest", StoryNameParser.parse(root + "/src/main/resources/br/materdei/bdd/feature/BddTest.estoria"));
-		assertEquals("br.materdei.bdd.feature.BddTest", StoryNameParser.parse(root + "/src/test/resources/br/materdei/bdd/feature/BddTest.estoria"));
-		assertEquals("br.materdei.bdd.feature.BddTest", StoryNameParser.parse("/src/test/resources/br/materdei/bdd/feature/BddTest.estoria"));
-		assertEquals("br.materdei.bdd.feature.BddTest", StoryNameParser.parse("br/materdei/bdd/feature/BddTest.estoria"));
+		assertEquals("br.materdei.bdd.feature.CadastraUsuarioTest", StoryNameParser.parse(root + "/src/main/resources/br/materdei/bdd/feature/cadastra_usuario.story"));
+		assertEquals("br.materdei.bdd.feature.CadastraUsuarioTest", StoryNameParser.parse(root + "/src/test/resources/br/materdei/bdd/feature/cadastra_usuario.story"));
+		assertEquals("br.materdei.bdd.feature.AtualizaUsuarioTest", StoryNameParser.parse(root + "/src/main/resources/br/materdei/bdd/feature/atualiza_usuario.estoria"));
+		assertEquals("br.materdei.bdd.feature.AtualizaUsuarioTest", StoryNameParser.parse(root + "/src/test/resources/br/materdei/bdd/feature/atualiza_usuario.estoria"));
+		assertEquals("br.materdei.bdd.feature.CadastraUsuarioTest", StoryNameParser.parse("/src/test/resources/br/materdei/bdd/feature/cadastra_usuario.estoria"));
+		assertEquals("br.materdei.bdd.feature.AtualizaUsuarioTest", StoryNameParser.parse("br/materdei/bdd/feature/atualiza_usuario.estoria"));
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void testParseWithInvalidFileName() {
+		String root = new File("").getAbsolutePath();
+		assertEquals("br.materdei.bdd.feature.CadastraUsuarioTest", StoryNameParser.parse(root + "/src/main/resources/br/materdei/bdd/feature/cadastraUsuario.story"));
+		assertEquals("br.materdei.bdd.feature.CadastraUsuarioTest", StoryNameParser.parse(root + "/src/main/resources/br/materdei/bdd/feature/CadastraUsuario.story"));
 	}
 }
