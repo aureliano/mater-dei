2e05362753ae2ed729e8fa31233d8df932b293fe
aureliano <aureliano.franca@hotmail.com>
2013-04-25 15:38:24 -0300

    Refatoração de teste.

--- src/test/java/br/materdei/bdd/jbehave/StoryFinderTest.java
+++ src/test/java/br/materdei/bdd/jbehave/StoryFinderTest.java
@@ -1,10 +1,13 @@
 package br.materdei.bdd.jbehave;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
 import java.util.List;
 
 import org.junit.Test;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
 
 public class StoryFinderTest {
 
@@ -13,5 +16,8 @@ public class StoryFinderTest {
 		List<String> stories = StoryFinder.find();
 		assertNotNull(stories);
 		assertEquals(2, stories.size());
+		
+		assertTrue(stories.contains(new File("src/test/resources/br/materdei/feature/atualiza_usuario.story").getAbsolutePath()));
+		assertTrue(stories.contains(new File("src/test/resources/br/materdei/feature/cadastra_usuario.estoria").getAbsolutePath()));
 	}
 }
