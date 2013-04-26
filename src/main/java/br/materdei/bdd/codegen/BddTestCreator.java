f9f8b97a1f3c788de89a3a0efbfc866cdf0f3a3c
aureliano <aureliano.franca@hotmail.com>
2013-04-26 10:50:28 -0300

    Obriga que a classe de teste seja StoryBase ou herde de StoryBase.

--- src/main/java/br/materdei/bdd/codegen/BddTestCreator.java
+++ src/main/java/br/materdei/bdd/codegen/BddTestCreator.java
@@ -1,8 +1,8 @@
 package br.materdei.bdd.codegen;
 
-import br.materdei.bdd.util.ClassUtil;
 import javassist.ClassPool;
 import javassist.CtClass;
+import br.materdei.bdd.util.ClassUtil;
 
 public final class BddTestCreator {
 
@@ -16,22 +16,25 @@ public final class BddTestCreator {
 		if (ClassUtil.classExists(storyName)) {
 			obj = Class.forName(storyName).newInstance();
 		} else {
-			ClassPool pool = ClassPool.getDefault();
-			if (storyBase == null) {
-				storyBase = StoryBase.class;
-			} else {
-				System.out.println(storyBase.getSuperclass());
-				if (!StoryBase.class.equals(storyBase.getSuperclass())) {
-					throw new RuntimeException(storyBase.getName() + " deve herdar de " + StoryBase.class.getName());
-				}
-			}
-			
-			CtClass ctClassStoryBase = pool.getCtClass(storyBase.getName());
-			CtClass evalClass = pool.makeClass(storyName, ctClassStoryBase);
-			
-			obj = evalClass.toClass().newInstance();
+			obj = createNewObject(storyBase, storyName);
 		}
 		
 		return obj;
+	}
+	
+	private static Object createNewObject(Class<?> storyBase, String storyName) throws Exception {
+		ClassPool pool = ClassPool.getDefault();
+		if (storyBase == null) {
+			storyBase = StoryBase.class;
+		} else {
+			if (!StoryBase.class.equals(storyBase.getSuperclass())) {
+				throw new RuntimeException(storyBase.getName() + " deve herdar de " + StoryBase.class.getName());
+			}
+		}
+		
+		CtClass ctClassStoryBase = pool.getCtClass(storyBase.getName());
+		CtClass evalClass = pool.makeClass(storyName, ctClassStoryBase);
+		
+		return evalClass.toClass().newInstance();
 	}
 }
