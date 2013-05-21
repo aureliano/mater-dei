6a26040994e011942722f2668b7205e0f3587ef2
aureliano <aureliano.franca@hotmail.com>
2013-05-21 17:49:06 -0300

    Refatoração de código.

--- src/main/java/br/materdei/bdd/codegen/StoryBase.java
+++ src/main/java/br/materdei/bdd/codegen/StoryBase.java
@@ -18,7 +18,7 @@ import br.materdei.bdd.TestModel;
 import br.materdei.bdd.TestRunnerHelper;
 import br.materdei.bdd.jbehave.WebDriverSingleton;
 import br.materdei.bdd.jbehave.config.JBehaveConfigurationUtil;
-import br.materdei.bdd.steps.WebSteps;
+import br.materdei.bdd.jbehave.steps.WebSteps;
 
 import com.google.common.util.concurrent.MoreExecutors;
 
