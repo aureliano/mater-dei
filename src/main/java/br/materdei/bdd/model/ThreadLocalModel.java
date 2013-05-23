897b8a7b671360bc6364850da19386ac70f04bfe
aureliano <aureliano.franca@hotmail.com>
2013-05-23 10:30:25 -0300

    Cria utilitário para aplicação de preferências ao web driver.

--- src/main/java/br/materdei/bdd/model/ThreadLocalModel.java
+++ src/main/java/br/materdei/bdd/model/ThreadLocalModel.java
@@ -5,6 +5,7 @@ public final class ThreadLocalModel {
 	private static final ThreadLocal<WebDriver> THREAD_LOCAL_WEB_DRIVER = new ThreadLocal<WebDriver>();
 	private static final ThreadLocal<Database> THREAD_LOCAL_DATABASE = new ThreadLocal<Database>();
 	private static final ThreadLocal<JBehave> THREAD_LOCAL_JBEHAVE = new ThreadLocal<JBehave>();
+	private static final ThreadLocal<Boolean> THREAD_LOCAL_WEB_DRIVER_PREFERENCES_DONE = new ThreadLocal<Boolean>();
 
 	private ThreadLocalModel() {
 		super();
@@ -32,5 +33,13 @@ public final class ThreadLocalModel {
 	
 	public static void setJBehaveModel(JBehave model) {
 		THREAD_LOCAL_JBEHAVE.set(model);
+	}
+	
+	public static void setWebDriverPreferencesDone(Boolean done) {
+		THREAD_LOCAL_WEB_DRIVER_PREFERENCES_DONE.set(done);
+	}
+	
+	public static Boolean getWebDriverPreferencesDone() {
+		return THREAD_LOCAL_WEB_DRIVER_PREFERENCES_DONE.get();
 	}
 }
---
+++ src/main/java/br/materdei/bdd/web/driver/WebDriverPreferences.java
@@ -0,0 +1,40 @@
+package br.materdei.bdd.web.driver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+
+import br.materdei.bdd.model.ThreadLocalModel;
+import br.materdei.bdd.model.WebDriver;
+
+public final class WebDriverPreferences {
+
+	private static final Logger logger = Logger.getLogger(WebDriverPreferences.class);
+	
+	public WebDriverPreferences() {
+		super();
+	}
+	
+	public static void applyPreferences() {
+		Boolean alreadyLoaded = ThreadLocalModel.getWebDriverPreferencesDone();
+		alreadyLoaded = (alreadyLoaded == null) ? false : alreadyLoaded.booleanValue();
+		
+		if (alreadyLoaded) {
+			return;
+		}		
+		
+		logger.info("Carregando preferências do web driver");
+		WebDriver model = ThreadLocalModel.getWebDriverModel();
+		org.openqa.selenium.WebDriver driver = WebDriverSingleton.get().getWebDriverProvider().get();
+		
+		logger.info("Web driver timeout: " + model.getDriverTimeout() + " segundos");
+		driver.manage().timeouts().implicitlyWait(model.getDriverTimeout(), TimeUnit.SECONDS);
+		
+		logger.info("Window maximized: " + model.isWindowMaximized());
+		if (model.isWindowMaximized()) {
+			driver.manage().window().maximize();
+		}
+		
+		ThreadLocalModel.setWebDriverPreferencesDone(true);
+	}
+}
