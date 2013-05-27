1788896e43f2bf7151bb13648ce19265504cf181
aureliano <aureliano.franca@hotmail.com>
2013-05-27 16:51:52 -0300

    Refatoração de código: cria helper para web steps.

--- src/main/java/br/materdei/bdd/jbehave/steps/WebSteps.java
+++ src/main/java/br/materdei/bdd/jbehave/steps/WebSteps.java
@@ -1,71 +1,61 @@
 package br.materdei.bdd.jbehave.steps;
 
+import static br.materdei.bdd.jbehave.steps.helper.WebStepsHelper.clickButton;
+import static br.materdei.bdd.jbehave.steps.helper.WebStepsHelper.clickLink;
+import static br.materdei.bdd.jbehave.steps.helper.WebStepsHelper.confirmModalMessage;
+import static br.materdei.bdd.jbehave.steps.helper.WebStepsHelper.dismissModalMessage;
+import static br.materdei.bdd.jbehave.steps.helper.WebStepsHelper.openPageByName;
+import static br.materdei.bdd.jbehave.steps.helper.WebStepsHelper.openPageByUrl;
+import static br.materdei.bdd.jbehave.steps.helper.WebStepsHelper.type;
+import static br.materdei.bdd.jbehave.steps.helper.WebStepsHelper.verifyPage;
+
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
-import org.jbehave.web.selenium.WebDriverProvider;
-import org.junit.Assert;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-
-import br.materdei.bdd.web.component.TextField;
-import br.materdei.bdd.web.driver.WebDriverSingleton;
-import br.materdei.bdd.web.page.POFinder;
 
 public class WebSteps {
-
-	private WebDriverProvider provider;
-	
-	public WebSteps() {
-		this.provider = WebDriverSingleton.get().getWebDriverProvider();
-	}
 	
 	@When("eu acesso a página pela url '$url'")
 	public void dadoQueEuAcessoPaginaPelaUrl(String url) {
-		this.provider.get().get(url);
+		openPageByUrl(url);
 	}
 	
 	@When("eu acesso a página '$pageName'")
 	public void dadoQueEuAcessoPagina(String pageName) {
-		POFinder.findByName(pageName).abrePagina();
+		openPageByName(pageName);
 	}
 	
 	@When("eu clico no botão '$buttonId' da página '$pageName'")
 	public void quandoEuClicoBotaoDaPagina(String buttonId, String pageName) {
-		POFinder.findByName(pageName).getComponent(buttonId).click();
+		clickButton(pageName, buttonId);
 	}
 	
 	@When("eu clico no botão '$button'")
 	public void quandoEuClicoBotao(String button) {
-		this.provider.get().findElement(By.xpath("//input[@value='" + button + "']")).click();
+		clickButton(button);
 	}
 	
 	@When("eu clico no link '$link'")
 	public void quandoEuClicoLink(String link) {
-		this.provider.get().findElement(By.xpath("//a[text()='" + link + "']")).click();
+		clickLink(link);
 	}
 	
 	@When("eu preencho o campo '$fieldId' da página '$pageName' com '$value'")
 	public void quandoEuPreenchoCampoDaPaginaComValor(String fieldId, String pageName, String value) {
-		TextField txtField = (TextField) POFinder.findByName(pageName).getComponent(fieldId);
-		txtField.type(value);
+		type(pageName, fieldId, value);
 	}
 	
 	@Then("eu devo ver a página '$pageName'")
 	public void entaoEuDevoVerPagina(String pageName) {
-		POFinder.findByName(pageName).verificaPagina();
+		verifyPage(pageName);
 	}
 	
 	@When("eu aceito a mensagem de confirmação '$message'")
 	public void quandoEuAceitoMensagemConfirmacao(String message) {
-		Alert javascriptAlert = this.provider.get().switchTo().alert();
-	    Assert.assertEquals(message, javascriptAlert.getText());
-	    javascriptAlert.accept();
+		confirmModalMessage(message);
 	}
 	
 	@When("eu rejeito a mensagem de confirmação '$message'")
 	public void quandoEuRejeitoMensagemConfirmacao(String message) {
-		Alert javascriptAlert = this.provider.get().switchTo().alert();
-	    Assert.assertEquals(message, javascriptAlert.getText());
-	    javascriptAlert.dismiss();
+		dismissModalMessage(message);
 	}
 }
---
+++ src/main/java/br/materdei/bdd/jbehave/steps/helper/WebStepsHelper.java
@@ -0,0 +1,58 @@
+package br.materdei.bdd.jbehave.steps.helper;
+
+import org.junit.Assert;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import br.materdei.bdd.web.component.TextField;
+import br.materdei.bdd.web.driver.WebDriverSingleton;
+import br.materdei.bdd.web.page.POFinder;
+
+public final class WebStepsHelper {
+
+	public static void openPageByUrl(String url) {
+		driver().get(url);
+	}
+	
+	public static void openPageByName(String pageName) {
+		POFinder.findByName(pageName).abrePagina();
+	}
+	
+	public static void clickButton(String pageName, String buttonId) {
+		POFinder.findByName(pageName).getComponent(buttonId).click();
+	}
+	
+	public static void clickButton(String buttonLabel) {
+		driver().findElement(By.xpath("//input[@value='" + buttonLabel + "']")).click();
+	}
+	
+	public static void clickLink(String link) {
+		driver().findElement(By.xpath("//a[text()='" + link + "']")).click();
+	}
+	
+	public static void type(String pageName, String fieldId, String value) {
+		TextField txtField = (TextField) POFinder.findByName(pageName).getComponent(fieldId);
+		txtField.type(value);
+	}
+	
+	public static void verifyPage(String pageName) {
+		POFinder.findByName(pageName).verificaPagina();
+	}
+	
+	public static void confirmModalMessage(String message) {
+		Alert javascriptAlert = driver().switchTo().alert();
+	    Assert.assertEquals(message, javascriptAlert.getText());
+	    javascriptAlert.accept();
+	}
+	
+	public static void dismissModalMessage(String message) {
+		Alert javascriptAlert = driver().switchTo().alert();
+	    Assert.assertEquals(message, javascriptAlert.getText());
+	    javascriptAlert.dismiss();
+	}
+	
+	private static WebDriver driver() {
+		return WebDriverSingleton.get().getWebDriverProvider().get();
+	}
+}
